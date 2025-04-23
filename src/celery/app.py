3c36a1b1a6ffabd7f14daebf5bd5967e9677f95f
GAKiknadze <gakiknadze@yandex.ru>
2025-04-23 12:15:03 +0300

    Added celery signal and broker config

--- src/celery/app.py
+++ src/celery/app.py
@@ -1,5 +1,13 @@
-from celery import Celery
+from asgiref.sync import async_to_sync
+
+from celery import Celery, signals
 
 from ..core.config import Config
+from ..core.database import init_engine
 
 app = Celery(broker=Config.celery.broker)
+
+
+@signals.worker_process_init.connect
+def on_start(*args, **kwargs):
+    async_to_sync(init_engine)(Config.db.uri)
